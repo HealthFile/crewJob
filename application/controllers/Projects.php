725e21c2d36d389eef641c10da0afebdba057f37
Петър Петров <ipetrovbg@gmail.com>
2016-04-16 13:56:52 +0300

    projects model and controller

--- application/controllers/Projects.php
+++ application/controllers/Projects.php
@@ -5,11 +5,23 @@ class Projects extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('Category_model');
         $this->load->model('Projects_model');
     }
     public function index()
     {
-        $category = $this->Projects_model->getAllCategory();
+        $category = $this->Category_model->getAllCategory();
+        $projects = $this->Projects_model->getAllProjects();
+        echo "<h1>Projects:</h1>";
+//        foreach ($projects as $project) {
+//            echo $project['name'];
+//        }
+        echo "<pre>";
+        print_r($projects);
+        echo "</pre>";
+
+        echo "<hr/>";
+        echo "<h1>Categories:</h1>";
         foreach ($category as $item) {
             echo $item['name']. '<br />';
         }
@@ -17,10 +29,11 @@ class Projects extends CI_Controller
 
     public function show($id)
     {
-        $cat = $this->Projects_model->getByID($id);
-
-        if($cat){
-            echo $cat[0]['name'];
+        $project = $this->Projects_model->getByID($id);
+        if($project){
+            echo "<pre>";
+            print_r($project);
+            echo "</pre>";
         }
 
     }
--- application/models/Projects_model.php
+++ application/models/Projects_model.php
@@ -3,18 +3,22 @@
 
 class Projects_model extends CI_Model
 {
-    public function getAllCategory()
+    public function getAllProjects()
     {
-        $cat = $this->db->select('*')
-            ->get('category');
-        return $cat->result_array();
+        $projects = $this->db->select('cat.name AS cat_name, cat.id AS cat_ID, pr.name AS pr_name, pr.id AS pr_ID')
+            ->join('projects AS pr', 'pr.id = rel.project_ID')
+            ->join('category AS cat', 'cat.id = rel.category_ID')
+            ->get('project_category_rel AS rel');
+        return $projects->result_array();
     }
 
     public function getByID($id)
     {
-        $cat = $this->db->select('*')
-                ->where('id', $id)
-                ->get('category');
-        return $cat->result_array();
+        $project = $this->db->select('cat.name AS cat_name, cat.id AS cat_ID, pr.name AS pr_name, pr.id AS pr_ID')
+            ->join('projects AS pr', 'pr.id = rel.project_ID', 'left')
+            ->join('category AS cat', 'cat.id = rel.category_ID', 'left')
+            ->where('rel.project_ID', $id)
+            ->get('project_category_rel AS rel');
+        return $project->result_array();
     }
 }
