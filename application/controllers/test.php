bc9877bdca34a90416c6391f3e291c60f9f91c0b
Петър Петров <ipetrovbg@gmail.com>
2016-04-17 11:09:42 +0300

    create projects views

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -7,7 +7,7 @@ class Test extends CI_Controller
 	
 	public function index(){
 		$this->load->view('header');
-		$this->load->view('user-settings');
+		$this->load->view('create-project');
 		$this->load->view('footer');
 	}
 }
