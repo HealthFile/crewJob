f96ac1703c8d55c2a1c18b6b28828bec50a5a50d
Петър Петров <ipetrovbg@gmail.com>
2016-04-17 12:10:40 +0300

    adding test

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -7,7 +7,7 @@ class Test extends CI_Controller
 	
 	public function index(){
 		$this->load->view('header');
-		$this->load->view('create-project');
+		$this->load->view('project-details');
 		$this->load->view('footer');
 		
 	}
