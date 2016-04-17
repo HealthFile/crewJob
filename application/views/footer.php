b29a7ea943feab4933cf220b12c9fc8306536694
Петър Петров <ipetrovbg@gmail.com>
2016-04-17 09:37:00 +0300

    html

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,17 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
   </div>
-  <p class="" style="text-align: center">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+  <footer class="footer">
+	  <div class="shell clearfix">
+	  		<p class="copyrights">Copyrights &copy; Web site by <a href="#">CrewJob</a>.</p>
+
+	  		<div class="social-links">
+		  		<a href="#"><i class="fa fa-facebook"></i></a>
+		  		<a href="#"><i class="fa fa-twitter"></i></a>
+		  		<a href="#"><i class="fa fa-google-plus"></i></a>
+		  		<a href="#"><i class="fa fa-linkedin"></i></a>
+	  		</div>
+	  </div>
+  </footer>
   <script src="assets/js/crewjob.js"></script>
 </body>
 </html>
-
